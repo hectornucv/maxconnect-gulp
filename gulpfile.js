695619bf0f42204faf76212fef65f0a03ae0f536
Alex <alexanderddking@gmail.com>
2015-03-20 13:19:31 +1000

    Update - No pacjkage json yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 'use strict';
 var 
   fileinclude = require('gulp-file-include'),
+    del = require('del'),
+   concat = require('gulp-concat'),
   gulp = require('gulp');
 
+
+//Include - includes html snippets
 gulp.task('include', function() {
   gulp.src(['cwd/templates/pages/*'])
     .pipe(fileinclude({
@@ -30,8 +34,21 @@ gulp.task('include', function() {
     .pipe(gulp.dest('./render/templates/layouts/'));
 });
 
+//Clean - delete everthing in render
+gulp.task('clean', function(cb) {
+    del(['./render/*'], cb)
+});
 
 
+//contact js -  
+ 
+gulp.task('scripts', function() {
+  return gulp.src('bower_components/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('render/assets/js/'));
+});
 
 
-gulp.task('default', ['include']);
+gulp.task('default', ['clean'], function() {
+   gulp.start('include','scripts');
+});
